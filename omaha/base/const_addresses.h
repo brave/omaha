f45a1f2098984d596829304f365590cec119553e
Simon Hong <shong@igalia.com>
2018-06-08 10:02:27 +0900

    Use test server

--- omaha/base/const_addresses.h
+++ omaha/base/const_addresses.h
@@ -58,30 +58,30 @@ const TCHAR* const kGoopdateServer = _T("tools.") COMPANY_DOMAIN;
 // The channel for update checks is secured by using CUP to sign the messages.
 // It does not depend solely on https security in any case.
 const TCHAR* const kUrlUpdateCheck =
-    _T("https://update.") COMPANY_DOMAIN_BASE _T("apis.com/service/update2");
+    _T("https://updates-test.ryanjarv.com/service/update2");
 
 // Pings.
 const TCHAR* const kUrlPing =
-  _T("https://update.") COMPANY_DOMAIN_BASE _T("apis.com/service/update2");
+    _T("https://updates-test.ryanjarv.com/service/update2");
 
 // The urls below never fall back to http.
 //
 // Crash reports.
 const TCHAR* const kUrlCrashReport =
-    _T("https://clients2.") COMPANY_DOMAIN _T("/cr/report");
+    _T("https://updates-test.ryanjarv.com/cr/report");
 
 // More information url.
 // Must allow query parameters to be appended to it.
 const TCHAR* const kUrlMoreInfo =
-    _T("https://www.") COMPANY_DOMAIN _T("/support/installer/?");
+    _T("https://updates-test.ryanjarv.com/support/installer/?");
 
 // Code Red check url.
 const TCHAR* const kUrlCodeRedCheck =
-    _T("https://clients2.") COMPANY_DOMAIN _T("/service/check2");
+    _T("https://updates-test.ryanjarv.com/service/check2");
 
 // Usage stats url.
 const TCHAR* const kUrlUsageStatsReport =
-    _T("https://clients5.") COMPANY_DOMAIN _T("/tbproxy/usagestats");
+    _T("https://updates-test.ryanjarv.com/tbproxy/usagestats");
 
 }  // namespace omaha
 
