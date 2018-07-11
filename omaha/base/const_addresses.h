ae7b5fa8d8ac9754120b9381ab168909af264abc
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2018-07-11 16:41:26 -0500

    Set new updater url

--- omaha/base/const_addresses.h
+++ omaha/base/const_addresses.h
@@ -58,30 +58,30 @@ const TCHAR* const kGoopdateServer = _T("tools.") COMPANY_DOMAIN;
 // The channel for update checks is secured by using CUP to sign the messages.
 // It does not depend solely on https security in any case.
 const TCHAR* const kUrlUpdateCheck =
-    _T("https://updates-test.bravesoftware.com/service/update2");
+    _T("https://updates.bravesoftware.com/service/update2");
 
 // Pings.
 const TCHAR* const kUrlPing =
-    _T("https://updates-test.bravesoftware.com/service/update2");
+    _T("https://updates.bravesoftware.com/service/update2");
 
 // The urls below never fall back to http.
 //
 // Crash reports.
 const TCHAR* const kUrlCrashReport =
-    _T("https://updates-test.bravesoftware.com/cr/report");
+    _T("https://updates.bravesoftware.com/cr/report");
 
 // More information url.
 // Must allow query parameters to be appended to it.
 const TCHAR* const kUrlMoreInfo =
-    _T("https://updates-test.bravesoftware.com/support/installer/?");
+    _T("https://updates.bravesoftware.com/support/installer/?");
 
 // Code Red check url.
 const TCHAR* const kUrlCodeRedCheck =
-    _T("https://updates-test.bravesoftware.com/service/check2");
+    _T("https://updates.bravesoftware.com/service/check2");
 
 // Usage stats url.
 const TCHAR* const kUrlUsageStatsReport =
-    _T("https://updates-test.bravesoftware.com/tbproxy/usagestats");
+    _T("https://updates.bravesoftware.com/tbproxy/usagestats");
 
 #if defined(HAS_DEVICE_MANAGEMENT)
 
