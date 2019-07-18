64d8c0899132146399f0470bd2876317944079e6
Simon Hong <shong@igalia.com>
2019-07-18 14:38:23 +0900

    Update hammer-brave.bat

--- omaha/hammer-brave.bat
+++ omaha/hammer-brave.bat
@@ -5,7 +5,7 @@
 set OMAHA_PSEXEC_DIR=%ProgramFiles(x86)%\pstools
 
 :: Set VS environment variables.
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 
 setlocal
 
@@ -18,10 +18,14 @@ rem -- Set all environment variables used by Hammer and Omaha. --
 :: VS2012/VC11 is 1700 (not supported by the current build).
 :: VS2013/VC12 is 1800.
 :: VS2015/VC14 is 1900.
+:: VS2017/VC14.1 is 1910.
+:: VS2019/VC16.0 is 1920.
 
 if "%VisualStudioVersion%"=="" goto error_no_vc
 if "%VisualStudioVersion%"=="12.0" goto vc120
 if "%VisualStudioVersion%"=="14.0" goto vc140
+if "%VisualStudioVersion%"=="15.0" goto vc141
+if "%VisualStudioVersion%"=="16.0" goto vc160
 goto error_vc_not_supported
 
 :vc120
@@ -30,6 +34,14 @@ goto set_env_variables
 
 :vc140
 set OMAHA_MSC_VER=1900
+goto set_env_variables
+
+:vc141
+set OMAHA_MSC_VER=1910
+goto set_env_variables
+
+:vc160
+set OMAHA_MSC_VER=1920
 goto set_env_variables
 
 :set_env_variables
@@ -57,7 +69,7 @@ set OMAHA_NET_DIR=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
 :: framework. If there is a need for the click-once application to run on older
 :: versions of the .Net framework, then an older version of the Windows SDK
 :: needs to be installed and this environment variable point to that directory.
-set OMAHA_NETFX_TOOLS_DIR=%WindowsSDK_ExecutablePath_x86%
+set OMAHA_NETFX_TOOLS_DIR=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.2 Tools
 
 :: This directory is needed to find protoc.exe, which is the protocol buffer
 :: compiler. From the release page https://github.com/google/protobuf/releases,
@@ -72,7 +84,7 @@ set OMAHA_PROTOBUF_BIN_DIR=C:\protobuf\bin
 set OMAHA_PROTOBUF_SRC_DIR=C:\protobuf\src
 
 :: Directory where Python (python.exe) is installed.
-set OMAHA_PYTHON_DIR=C:\Python24
+set OMAHA_PYTHON_DIR=C:\Python27
 
 :: Directory in WiX where candle.exe and light.exe are installed.
 set OMAHA_WIX_DIR=%ProgramFiles(x86)%\WiX Toolset v3.11\bin
@@ -84,11 +96,11 @@ set OMAHA_PLATFORM_SDK_DIR=%WindowsSdkDir%\
 set OMAHA_WINDOWS_SDK_10_0_VERSION=%WindowsSDKVersion:~0,-1%
 
 :: Directory which includes the sign.exe tool for Authenticode signing.
-set OMAHA_SIGNTOOL_SDK_DIR=%WindowsSdkDir%\bin\10.0.15063.0\x86
+set OMAHA_SIGNTOOL_SDK_DIR="%WindowsSdkVerBinPath%\x86"
 set PYTHONPATH=%OMAHA_PYTHON_DIR%
 
 :: Directory of Scons (http://www.scons.org/).
-set SCONS_DIR=C:\Python24\Lib\site-packages\scons-1.3.1
+set SCONS_DIR=C:\Python27\Lib\site-packages\scons-1.3.1
 
 :: Directory of the Google's Software Construction Toolkit.
 set SCT_DIR=C:\swtoolkit
@@ -96,12 +108,8 @@ set SCT_DIR=C:\swtoolkit
 set PROXY_CLSID_TARGET=%~dp0proxy_clsids.txt
 set CUSTOMIZATION_UT_TARGET=%~dp0common\omaha_customization_proxy_clsid.h
 
-
-set OMAHA_PSEXEC_DIR=C:\pstools
-
-rem Force Hammer to use Python 2.4.  (The default of Python 2.6 exposes some
-rem bugs in Scons 1.2, which we currently use.)
-set PYTHON_TO_USE=python_24
+rem Force Hammer to use Python 2.7
+set PYTHON_TO_USE=python_27
 call "%SCT_DIR%\hammer.bat" %*
 
 if /i {%1} == {-c} (
