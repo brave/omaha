ceb7c81d7b93c2674126965b7a0bbaf0932078e8
nguyenphl <phamholenguyen31@gmail.com>
2023-05-13 01:59:50 +0700

    update omaha url

--- omaha/base/const_addresses.h
+++ omaha/base/const_addresses.h
@@ -58,30 +58,30 @@ const TCHAR* const kGoopdateServer = _T("tools.") COMPANY_DOMAIN;
 // The channel for update checks is secured by using CUP to sign the messages.
 // It does not depend solely on https security in any case.
 const TCHAR* const kUrlUpdateCheck =
-    _T("https://omaha.herond.org/service/update2");
+    _T("https://updates.herond.org/service/update2");
 
 // Pings.
 const TCHAR* const kUrlPing =
-  _T("https://omaha.herond.org/service/update2");
+  _T("https://updates.herond.org/service/update2");
 
 // The urls below never fall back to http.
 //
 // Crash reports.
 const TCHAR* const kUrlCrashReport =
-    _T("https://omaha.herond.org/cr/report");
+    _T("https://updates.herond.org/cr/report");
 
 // More information url.
 // Must allow query parameters to be appended to it.
 const TCHAR* const kUrlMoreInfo =
-    _T("https://omaha.herond.org/support/installer/?");
+    _T("https://updates.herond.org/support/installer/?");
 
 // Code Red check url.
 const TCHAR* const kUrlCodeRedCheck =
-    _T("https://omaha.herond.org/service/check2");
+    _T("https://updates.herond.org/service/check2");
 
 // Usage stats url.
 const TCHAR* const kUrlUsageStatsReport =
-    _T("https://omaha.herond.org/tbproxy/usagestats");
+    _T("https://updates.herond.org/tbproxy/usagestats");
 
 #if defined(HAS_DEVICE_MANAGEMENT)
 
